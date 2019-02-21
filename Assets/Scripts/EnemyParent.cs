c004fc78b35d67ab09c7eb9f94017e79114798d2
Allerion2016 <44294557+Allerion2016@users.noreply.github.com>
2019-02-21 00:08:20 +0000

    Player Movement finished Enemy object created Collision isn't working May need to look into rigidbody2D

---
+++ Assets/Scripts/EnemyParent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyParent : MonoBehaviour {
+    [SerializeField] private int hp = 5;
+    [SerializeField] private int dmg = 1;
+    [SerializeField] private float spd = 0.1f;
+    [SerializeField] private bool alive = true;
+    [SerializeField] private int lvl = 1;
+    GameObject me;
+    // Start is called before the first frame update
+    void Start() {
+        me = new GameObject();
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    void die() {
+        if (hp == 0) {
+            Destroy(me);
+        }
+    }
+}
---
+++ Assets/Scripts/HeroCharacter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroCharacter : MonoBehaviour {
+    // Start is called before the first frame update
+
+    // initiating variables
+    [SerializeField] private int hp = 5;
+    [SerializeField] private int dmg = 1;
+    [SerializeField] private float spd = 32;
+    [SerializeField] private bool alive = true;
+    [SerializeField] private int lvl = 1;
+    [SerializeField] private int exp = 0;
+    GameObject Player;
+
+    void Start() {
+        Vector3 playerPos = transform.position;
+        Player = new GameObject();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        checkMovement();
+
+    }
+
+    void checkMovement() {
+        // Character movement
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            transform.position = new Vector3(transform.position.x + spd, transform.position.y, 0.0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            transform.position = new Vector3(transform.position.x - spd, transform.position.y, 0.0f);
+
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            transform.position = new Vector3(transform.position.x, transform.position.y + spd, 0.0f);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            transform.position = new Vector3(transform.position.x, transform.position.y - spd, 0.0f);
+        }
+    }
+
+    void OnCollisionEnter(Collision other) {
+     //TODO fix it
+    }
+}
