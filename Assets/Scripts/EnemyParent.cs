e3245f1b9996c3a35f78fa58aac0f604734bb29f
George Drakos <george.drakos97@gmail.com>
2019-11-28 18:37:16 +0000

    Constructor for enemy game object
    Created a constructor to be used when creating an enemy game object to be used for randomly generating location of enemies as well as increasing their health points / lvl.
    
    Also, some stats were changed to reflect the change of the game from an AI testing game to a boolean teaching game

--- Assets/Scripts/EnemyParent.cs
+++ Assets/Scripts/EnemyParent.cs
@@ -4,23 +4,22 @@ using UnityEngine;
 
 public class EnemyParent : MonoBehaviour {
     [SerializeField] private int hp = 5;
-    [SerializeField] private int dmg = 1;
-    [SerializeField] private float spd = 0.1f;
     [SerializeField] private bool alive = true;
     [SerializeField] private int lvl = 1;
 
     public int Hp { get => hp; set => hp = value; }
-    public int Dmg { get => dmg; set => dmg = value; }
-    public float Spd { get => spd; set => spd = value; }
     public bool Alive { get => alive; set => alive = value; }
     public int Lvl { get => lvl; set => lvl = value; }
 
     GameObject me;
-    // Start is called before the first frame update
-    void Start() {
-        me = new GameObject();
+    // Constructor for enemies / will assist in randomizing position of the enemies
+    public EnemyParent(int x,int y, int hp, bool alive, int lvl) {
+        //x = x position of enemy
+        // y = y position of enemy
+        hp = this.hp;
+        alive = this.alive;
+        lvl = this.lvl;
     }
-
     // Update is called once per frame
     void Update() {
             if (hp <= 0) {
