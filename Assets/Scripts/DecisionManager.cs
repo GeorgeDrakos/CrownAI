c6bcecbd791abebd02642be43bebb3472726c29e
George Drakos <george.drakos97@gmail.com>
2019-12-08 20:43:52 +0000

    Rescaled,Battle Scene working with hard coded example
    Had to increase the size of everything because I ran into problems using the Canvas feature of Unity for the UI because of how small the "assets" were.
    
    The Battle scene now shows a boolean expression and waits for the user to select true or false and then depending on the user's actions a debug message appears saying whether the answer was correct or not

---
+++ Assets/Scripts/DecisionManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DecisionManager : MonoBehaviour {
+
+
+    public void True() {
+        Debug.Log("Nope, the answer is false");
+
+    }
+
+    public void False() {
+        Debug.Log("Correct!");
+    }
+}
---
+++ Assets/Scripts/QuestionDisplay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestionDisplay : MonoBehaviour {
+    public Text question;
+    public bool answer;
+
+    void Start() {
+        //TODO: implement a way to select a different question everytime combat happens
+        question.text = "5 < 5";
+        answer = 5 < 5;
+    }
+}
