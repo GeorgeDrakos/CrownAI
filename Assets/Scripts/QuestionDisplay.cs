fa058a2ad0571799e1b2ca1d48fdcabb18265919
George Drakos <george.drakos97@gmail.com>
2020-02-11 18:22:04 +0000

    (WIP) Making question decision work witth an array
    Getting an error on Question display

--- Assets/Scripts/QuestionDisplay.cs
+++ Assets/Scripts/QuestionDisplay.cs
@@ -5,11 +5,13 @@ using UnityEngine.UI;
 
 public class QuestionDisplay : MonoBehaviour {
     public Text question;
-    public bool answer;
-
-    void Start() {
+    public string n;
+    void Awake() {
         //TODO: implement a way to select a different question everytime combat happens
-        question.text = "5 < 5";
-        answer = 5 < 5;
+        //Populating the array with questions
+        string[,] Questions = new string[2,2] { { "5 < 5", "False" }, { "8 < 23", "True" } } ;
+        string n = (Random.Range(0, 2).ToString());
+        Debug.Log(Questions(n,0));
+        //question.setText = Questions[n, 0];
     }
 }
--- Assets/Scripts/SpawnEnemies.cs
+++ Assets/Scripts/SpawnEnemies.cs
@@ -8,16 +8,12 @@ public class SpawnEnemies : MonoBehaviour {
     public int enemyDifficulty;
     // Start is called before the first frame update
     void Start() {
-        //for (int x = 0; x == enemyDifficulty; x++) {
-        //Vector3 position = new Vector3(Random.Range(-10.0f, 10.0f), 0 ,0);
-        //Instantiate(enemyPrefab, position, Quaternion.identity);
-        //Debug.Log("Enemy instantiated");
-        //Debug.Log("enemy number" + x);
-        //detecting all enemies for debug purposes
-        //}
 
         for (int i = 0; i < enemyDifficulty; i++) {
             Instantiate(enemyPrefab, new Vector3(Random.Range(1f, 750f), Random.Range(1f, 340f), 0), Quaternion.identity);
+            Debug.Log("Enemy instantiated");
+            Debug.Log("enemy number: " + i);
+            //detecting all enemies for debug purposes
         }
     }
 
