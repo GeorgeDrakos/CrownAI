b7fb5b4b85e168d7bb608df8d210b33d4911a905
George Drakos <george.drakos97@gmail.com>
2020-02-18 11:29:57 +0000

    QuestionGeneration works!!!
    FInished question generator and it now accurately constructs different questions everytime the fight scene is accessed.

---
+++ Assets/Scripts/QuestionChooser.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestionChooser : MonoBehaviour
+{
+    Text questionText;
+    Text score;
+    QuestionGen newQ;
+    QuestionGen exampleQ;
+
+    void Start() {
+        newQ = generateQuestion();
+        //newQ = new QuestionGen(1,2,'<',true);
+        Debug.Log(newQ.toString());
+        questionDisplay(newQ);
+    }
+    public void questionDisplay(QuestionGen generatedQ) {
+        questionText = GameObject.Find("Question").GetComponent<Text>();
+        questionText.text = generatedQ.toString();
+        score = GameObject.Find("Score").GetComponent<Text>();
+
+    }
+    public void answerTrue() {
+        if (newQ.answer == true) {
+            Debug.Log("This is correct, you win!");
+
+        } else {
+            Debug.Log("This is wrong :(");
+
+        }
+    }
+    public void answerFalse() {
+        if (newQ.answer == false) {
+            Debug.Log("This is correct, you win!");
+
+        } else {
+            Debug.Log("This is wrong :(");
+        }
+    }
+
+    public QuestionGen generateQuestion() {
+
+         int LHS = Random.Range(1, 100);
+         int RHS = Random.Range(2, 100);
+
+        if (LHS > RHS) {
+            LHS = Random.Range(1, 100);
+            RHS = Random.Range(2, 100);
+        }
+        else {
+           exampleQ = new QuestionGen(LHS, RHS, '<', true);
+        }
+        return exampleQ;
+    }
+}
---
+++ Assets/Scripts/QuestionGen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class QuestionGen {
+        public int LHS { get; set; }
+        public int RHS { get; set; }
+        public char op { get; set; }
+        public bool answer { get; set; }
+
+    public QuestionGen(int LHS, int RHS, char op, bool answer) {
+            this.LHS = LHS;
+            this.RHS = RHS;
+            this.op = op;
+            this.answer = answer;
+        }
+
+        public string toString() {
+            return LHS.ToString() + op.ToString() + RHS.ToString();
+        }
+    }
